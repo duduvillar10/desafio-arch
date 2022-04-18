605a4686be6ff3359bf2a187eaff5085231851e1
duduvillar10 <duduvillar10io@hotmail.com>
2022-04-18 11:09:46 -0300

    feat: async errors at transacao-api

--- transacao-api/src/infra/http/app.ts
+++ transacao-api/src/infra/http/app.ts
@@ -1,8 +1,10 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { router } from "./routes";
 import createMongoConnection from "../database/mongodb";
 import "../container";
+import { AppError } from "../errors/AppError";
 
 createMongoConnection();
 
@@ -11,5 +13,20 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
