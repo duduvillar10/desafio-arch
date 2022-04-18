c1955b12c2479d0a85cb26447d9585375aa29d0d
duduvillar10 <duduvillar10io@hotmail.com>
2022-04-18 13:01:58 -0300

    test: statement

--- transacao-api/src/application/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ transacao-api/src/application/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,13 +1,35 @@
+import { InMemoryRedisRepository } from "../../repositories/in-memory/InMemoryRedisRepository";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 let createStatementUseCase: CreateStatementUseCase;
 let inMemoryStatementsRepository: InMemoryStatementsRepository;
+let inMemoryRedisRepository;
 
 describe("Statement", () => {
   beforeEach(() => {
     inMemoryStatementsRepository = new InMemoryStatementsRepository();
-    // createStatementUseCase = new CreateStatementUseCase(inMemoryStatementsRepository,);
+    inMemoryRedisRepository = new InMemoryRedisRepository();
+    createStatementUseCase = new CreateStatementUseCase(
+      inMemoryStatementsRepository,
+      inMemoryRedisRepository
+    );
   });
-  it("should be able to create a statemtent", () => {});
+
+  it("should be able to create a statement", async () => {
+    const statement = await createStatementUseCase.execute({
+      user_id: "1341",
+      amount: 134,
+      date: new Date(),
+      type: "deposit",
+    });
+
+    const statements = await inMemoryStatementsRepository.getStatementsById(
+      "1341"
+    );
+
+    expect(statements).toEqual([statement]);
+    expect(statement).toHaveProperty("_id");
+    expect(statement.user_id).toEqual("1341");
+  });
 });
